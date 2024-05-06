8bd99428957052d76ae8cb568fa8ee39e29417be
samirkarimov022 <samir.karimov0022@gmail.com>
2024-05-07 02:54:08 +0400

    add

---
+++ Pronia/ProniaApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProniaApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index() 
+        {
+            return View();
+        }    
+        public async Task<IActionResult> Index2() 
+        {
+            return View();
+        }
+        public async Task<IActionResult> Shop() 
+        {
+            return View();
+        }
+        public async Task<IActionResult> Blog() 
+        {
+            return View();
+        }
+        public async Task<IActionResult> About() 
+        {
+            return View();
+        }
+        public async Task<IActionResult> Contact() 
+        {
+            return View();
+        }
+    }
+}
